537141ea7bfa65cd94f657fe8f34feb821448929
altalerico <altalerico@gmail.com>
2014-08-27 16:54:14 -0400

    Application Parser

---
+++ parse_application.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>Parse Application</title>
+</head>
+
+<body>
+	<?php
+	?>
+	<form method="post" action = "print_application.php">
+	<input type="submit" value="Parse"><br>
+	<label 	for="text_to_parse" 
+			class="profile_label"
+			style = "vertical-align: top;">Paste text here:
+	</label>
+	<textarea name="text_to_parse" id="text_to_parse" rows="40" cols="32">
+		
+	</textarea>
+
+	</form>
+<?php
+
+?>
+</body>
+</html>
